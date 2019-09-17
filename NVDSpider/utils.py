82e49f22ac6161b6249b4417b167f620ad5d0dcd
bertranwang <bowdenwong@outlook.com>
2019-09-17 19:51:09 +0800

    clear

--- NVDSpider/utils.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# 工具函数
-
-from datetime import datetime
-
-def parseDate(s):
-    mon_s, day_s, year_s = s.split('/')
-    return datetime(int(year_s), int(mon_s), int(day_s))
