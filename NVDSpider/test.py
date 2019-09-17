fb1a0530f90c26d259b045c39dc372024b5e5801
bertranwang <bowdenwong@outlook.com>
2019-09-17 21:17:52 +0800

    开始初始配置

--- NVDSpider/test.py
+++ NVDSpider/test.py
@@ -5,15 +5,30 @@ from NVDSpider.settings import MONGO_HOST
 from NVDSpider.settings import MONGO_PORT
 from NVDSpider.settings import MONGO_DB
 from NVDSpider.settings import MONGO_ID
+from scrapy import cmdline
+from NVDSpider.mongoSync import mongoSync
 
 if __name__ == "__main__":
-    start_urls = []
-    client = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
-    db = client[MONGO_DB]
-    coll = db[MONGO_ID]
-    results = coll.find()
-    for item in results:
-        url = item['vuln_url']
-        start_urls.append(url)
-    client.close()
-    print(start_urls)
+
+    # 测试爬虫
+    # cmdline.execute('scrapy crawl increSpider'.split())
+    cmdline.execute('scrapy crawl detailSpider'.split())
+    # cmdline.execute('scrapy crawl testSpider'.split())
+
+    # 测试初步配置
+    mongo = mongoSync()
+    path = "../data/"
+    mongo.loadFile(path)
+
+
+    # 测试数据库
+    # start_urls = []
+    # client = pymongo.MongoClient(MONGO_HOST, MONGO_PORT)
+    # db = client[MONGO_DB]
+    # coll = db[MONGO_ID]
+    # results = coll.find()
+    # for item in results:
+    #     url = item['vuln_url']
+    #     start_urls.append(url)
+    # client.close()
+    # print(start_urls)
